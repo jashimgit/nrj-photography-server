dc723934d3e9ec7c8e5602d0aa0dddac97b74acd
jashimgit <jashim.ncn@gmail.com>
2021-08-06 19:45:10 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## NRJ Photography serve code
+
+
+## Technology used
+	1. Express JS
+	2. Node JS
+	3. Mongodb
---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 5000;
+require("dotenv").config();
+const { MongoClient } = require('mongodb');
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let ObjectId = require("mongodb").ObjectID;
+
+
+const uri = "mongodb+srv://nodemongo:modemongo@cluster0.vewnd.mongodb.net/nrjPhotography?retryWrites=true&w=majority";
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+  if(err) throw err;
+
+  const orderCollection = client.db("nrjPhotography").collection("orders");
+  const userCollection = client.db("nrjPhotography").collection("users");
+  // perform actions on the collection object
+
+  // create an admin
+  app.post("/add-admin", (req,res) => {
+    const userDetails = req.body;
+    userCollection.insertOne(userDetails)
+    .then((result) => {
+      res.send(result.insertedId);
+    })
+    .catch(err => console.log(err))
+  })
+
+  // get all orders
+  app.get("/orders", (req, res) => {
+    orderCollection.find({})
+      .toArray((err, doc) => {
+        res.send(doc);
+      });
+  });
+
+  // add order 
+  app.post("/add-order", (req, res) => {
+    const orderDetails = req.body;
+    // console.log(orderDetails);
+    orderCollection.insertOne(orderDetails)
+      .then((result) => { 
+        res.send(result.insertedId);
+      })
+      .catch(err => console.log('err :' , err))
+  })
+
+  // change service status 
+
+   app.put('/change-status', (req, res) => {
+    const status = req.body.status;
+    const id = req.body._id;
+    // console.log(id);
+    orderCollection.updateOne(
+      {_id: ObjectId(id)},
+      {$set: {status: status}}
+      )
+    .then(result => {
+      res.send(result.modifiedCount > 0)
+    })
+   })
+
+  // client.close();
+
+  app.get("/", (req, res) => {
+    res.send('<h1>server is working</h1>');
+  })
+  
+  
+  
+  app.listen(PORT, () => {
+    console.log(`server is runnning on port ${PORT}`);
+  });
+})
+
+
